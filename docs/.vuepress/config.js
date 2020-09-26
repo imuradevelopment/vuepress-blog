bb29632dbe854b5183555002d12d3db01daeb4d6
imuradevelopment <imuradevelopmentauth@gmail.com>
2020-09-26 22:57:10 +0900

    ブログ構築

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,4 +13,33 @@ module.exports = {
     lineNumbers: true,
     toc: { includeLevel: [1, 2, 3] },
   },
+  locales: {
+    "/": {
+      lang: "ja",
+      title: "ADHDの休憩所",
+      description: " メモ用ブログ ",
+      head: [
+        //        ['meta', { name: 'keywords', content: 'xxx, vueブログ' }],
+        //        ['meta', { property: 'og:title', content: 'xxx' }],
+        [
+          //  "meta",
+          {
+            //    name: "og:description",
+            //    content: " Description",
+          },
+        ],
+        //["meta", { property: "og:locale", content: "ja" }],
+        //["meta", { property: "og:type", content: "website" }],
+        //["meta", { property: "og:url", content: "https://xxx.com" }],
+        //["meta", { property: "og:site_name", content: "xxx Blog" }],
+        [
+          //  "meta",
+          {
+            //    property: "og:image",
+            //    content: "https://xxx.com/images/icons/icon-192x192.png",
+          },
+        ],
+      ],
+    },
+  },
 };
