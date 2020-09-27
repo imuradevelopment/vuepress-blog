254991340410c9e8eaf7c9d0a4edaa403c54b0aa
imuradevelopment <imuradevelopmentauth@gmail.com>
2020-09-27 21:20:39 +0900

    ブログ更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,26 +36,26 @@ module.exports = {
     searchMaxSuggestions: 10,
     searchPlaceholder: "検索...",
     lastUpdated: "最終更新",
-    nextLinks: false,
-    prevLinks: false,
-    // Assumes GitHub. Can also be a full GitLab url.
-    repo: "imuradevelopment/vuepress-blog",
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-    repoLabel: "助ける！",
+    nextLinks: true,
+    prevLinks: true,
+    // // Assumes GitHub. Can also be a full GitLab url.
+    // repo: "imuradevelopment/vuepress-blog",
+    // // Customising the header label
+    // // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    // repoLabel: "助ける！",
 
-    // Optional options for generating "Edit this page" link
+    // // Optional options for generating "Edit this page" link
 
-    // if your docs are in a different repo from your main project:
-    docsRepo: "vuejs/vuepress",
-    // if your docs are not at the root of the repo:
-    docsDir: "docs",
-    // if your docs are in a specific branch (defaults to 'master'):
-    docsBranch: "Setup",
-    // defaults to false, set to true to enable
-    editLinks: true,
-    // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: "このページの改善にご協力ください。",
+    // // if your docs are in a different repo from your main project:
+    // docsRepo: "vuejs/vuepress",
+    // // if your docs are not at the root of the repo:
+    // docsDir: "docs",
+    // // if your docs are in a specific branch (defaults to 'master'):
+    // docsBranch: "Setup",
+    // // defaults to false, set to true to enable
+    // editLinks: true,
+    // // custom text for edit link. Defaults to "Edit this page"
+    // editLinkText: "このページの改善にご協力ください。",
     locales: {
       "/": {
         // // text for the language dropdown
